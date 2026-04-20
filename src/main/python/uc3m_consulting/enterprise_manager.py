c2be4c1f3ee4ce97daa70840870360209825fa3a
Linus Munn <lmunn@umass.edu>
2026-04-20 10:52:57 +0200

    Mysterious Names - Refactored parameter name t_d of validate_starting_date into starting_date for clarity

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -61,15 +61,15 @@ class EnterpriseManager:
             raise EnterpriseManagementException("CIF type not supported")
         return True
 
-    def validate_starting_date(self, t_d):
+    def validate_starting_date(self, starting_date):
         """validates the  date format  using regex"""
         date_pattern = re.compile(r"^(([0-2]\d|3[0-1])\/(0\d|1[0-2])\/\d\d\d\d)$")
-        match = date_pattern.fullmatch(t_d)
+        match = date_pattern.fullmatch(starting_date)
         if not match:
             raise EnterpriseManagementException("Invalid date format")
 
         try:
-            parsed_date = datetime.strptime(t_d, "%d/%m/%Y").date()
+            parsed_date = datetime.strptime(starting_date, "%d/%m/%Y").date()
         except ValueError as ex:
             raise EnterpriseManagementException("Invalid date format") from ex
 
@@ -78,7 +78,7 @@ class EnterpriseManager:
 
         if parsed_date.year < 2025 or parsed_date.year > 2050:
             raise EnterpriseManagementException("Invalid date format")
-        return t_d
+        return starting_date
     #pylint: disable=too-many-arguments, too-many-positional-arguments
     def register_project(self,
                          company_cif: str,
