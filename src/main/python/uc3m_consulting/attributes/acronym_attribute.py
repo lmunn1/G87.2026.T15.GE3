29eba3b4630d2a13418a82420436d4eed845a825
Linus Munn <lmunn@umass.edu>
2026-04-22 22:24:10 +0200

    Refactored attribute.py to require each field instead of checking for None. acronym's attribute class was also refactored accordingly.

--- src/main/python/uc3m_consulting/attributes/acronym_attribute.py
+++ src/main/python/uc3m_consulting/attributes/acronym_attribute.py
@@ -4,8 +4,9 @@ from uc3m_consulting.attributes.attribute import Attribute
 class ProjectAcronym(Attribute):
     """Definition of attribute acronym class"""
 
-    # pylint: disable=super-init-not-called
     def __init__(self, attr_value):
-        self._validation_pattern = r"^[a-zA-Z0-9]{5,10}$"
-        self._error_message = "Invalid acronym"
-        self._attr_value = self.validate(attr_value)
+        super().__init__(
+            attr_value,
+            r"^[a-zA-Z0-9]{5,10}$",
+            "Invalid acronym"
+        )
